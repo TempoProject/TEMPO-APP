de7d201a224f3e5d9619ddb13a8046b8ab890d61
Francesco Franco <frafranco16@gmail.com>
2024-03-08 16:47:21 +0100

    Add BleedingEventViewModel

---
+++ app/src/main/java/com/tempo/tempoapp/ui/bleeding/BleedingEventViewModel.kt
@@ -0,0 +1,69 @@
+package com.tempo.tempoapp.ui.bleeding
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.tempo.tempoapp.data.model.BleedingEvent
+import com.tempo.tempoapp.data.repository.BleedingRepository
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class BleedingEventViewModel(private val bleedingRepository: BleedingRepository) : ViewModel() {
+
+
+    var uiState by mutableStateOf(BleedingEventUiState())
+        private set
+
+
+    fun updateUiState(bleedingDetails: BleedingDetails) {
+        uiState = BleedingEventUiState(bleedingDetails, validateInput(bleedingDetails))
+    }
+
+    fun reset() {
+        uiState = BleedingEventUiState(BleedingDetails())
+    }
+
+    // fix severity button error
+    private fun validateInput(bleedingDetails: BleedingDetails = uiState.bleedingDetails): Boolean {
+        return with(bleedingDetails) {
+            site.isNotBlank()
+                    && cause.isNotBlank()
+                    && painScale.isNotBlank()
+                    //&& severity.isNotBlank()
+                    && date.isNotBlank()
+                    && time.isNotBlank()
+
+        }
+    }
+
+    suspend fun onSave() {
+        if (validateInput())
+            bleedingRepository.insertItem(uiState.bleedingDetails.toEntity())
+    }
+}
+
+data class BleedingEventUiState(
+    val bleedingDetails: BleedingDetails = BleedingDetails(),
+    val isEntryValid: Boolean = false
+)
+
+data class BleedingDetails(
+    val site: String = "",
+    val cause: String = "",
+    val severity: String = "",
+    val painScale: String = "",
+    val note: String = "",
+    val date: String = SimpleDateFormat("dd-MM-yyyy").format(Date()).toString(),
+    val time: String = SimpleDateFormat("HH:mm").format(Date()),
+)
+
+fun BleedingDetails.toEntity(): BleedingEvent =
+    BleedingEvent(
+        bleedingSite = site,
+        bleedingCause = cause,
+        severity = severity,
+        painScale = painScale,
+        note = note,
+        timestamp = SimpleDateFormat("dd-MM-yyyy HH:mm").parse(date.plus(" $time")).time
+    )
