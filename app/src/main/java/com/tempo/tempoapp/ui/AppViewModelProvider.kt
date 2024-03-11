f724004eaaaff524f0dd8231117d0136c4b8093e
Francesco Franco <frafranco16@gmail.com>
2024-03-11 16:41:24 +0100

    Add BleedingDetailsViewModel and BleedingEditViewModel + refactoring name from BleedingEventViewModel to BleedingEntryViewModel

--- app/src/main/java/com/tempo/tempoapp/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/tempo/tempoapp/ui/AppViewModelProvider.kt
@@ -1,21 +1,36 @@
 package com.tempo.tempoapp.ui
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.tempo.tempoapp.TempoApplication
-import com.tempo.tempoapp.ui.bleeding.BleedingEventViewModel
+import com.tempo.tempoapp.ui.bleeding.BleedingDetailsViewModel
+import com.tempo.tempoapp.ui.bleeding.BleedingEditViewModel
+import com.tempo.tempoapp.ui.bleeding.BleedingEntryViewModel
 import com.tempo.tempoapp.ui.home.HomeViewModel
 
 object AppViewModelProvider {
 
     val Factory = viewModelFactory {
         initializer {
-            BleedingEventViewModel(tempoApplication().container.bleedingRepository)
+            BleedingEntryViewModel(tempoApplication().container.bleedingRepository)
         }
         initializer {
             HomeViewModel(tempoApplication().container.bleedingRepository)
+        }
+        initializer {
+            BleedingDetailsViewModel(
+                this.createSavedStateHandle(),
+                tempoApplication().container.bleedingRepository
+            )
+        }
+        initializer {
+            BleedingEditViewModel(
+                this.createSavedStateHandle(),
+                tempoApplication().container.bleedingRepository
+            )
         }
     }
 }
