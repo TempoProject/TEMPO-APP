8746b6be73075b824d2a58f76ad3cb57e15aeec7
Francesco Franco <frafranco16@gmail.com>
2024-03-11 16:41:43 +0100

    Add navigation

--- app/src/main/java/com/tempo/tempoapp/ui/navigation/TempoNavGraph.kt
+++ app/src/main/java/com/tempo/tempoapp/ui/navigation/TempoNavGraph.kt
@@ -3,10 +3,16 @@ package com.tempo.tempoapp.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tempo.tempoapp.ui.bleeding.BleedingEventDestination
-import com.tempo.tempoapp.ui.bleeding.BleedingEventScreen
+import androidx.navigation.navArgument
+import com.tempo.tempoapp.ui.bleeding.BleedingDetailsScreen
+import com.tempo.tempoapp.ui.bleeding.BleedingEditScreen
+import com.tempo.tempoapp.ui.bleeding.BleedingEntryDestination
+import com.tempo.tempoapp.ui.bleeding.BleedingEntryScreen
+import com.tempo.tempoapp.ui.bleeding.BleedingEventDetailsDestination
+import com.tempo.tempoapp.ui.bleeding.BleedingEventEditDestination
 import com.tempo.tempoapp.ui.home.HomeDestination
 import com.tempo.tempoapp.ui.home.HomeScreen
 
@@ -16,17 +22,51 @@ fun TempoNavHost(
     modifier: Modifier = Modifier,
 ) {
     NavHost(
-        navController = navController,
-        startDestination = HomeDestination.route,
-        modifier = modifier
+        navController = navController, startDestination = HomeDestination.route, modifier = modifier
     ) {
+        /**
+         * Home screen
+         */
         composable(route = HomeDestination.route) {
-            HomeScreen(navigateToBleedingEntry = { navController.navigate(BleedingEventDestination.route) })
+            HomeScreen(navigateToBleedingEntry = { navController.navigate(BleedingEntryDestination.route) },
+                navigateToBleedingUpdate = {
+                    navController.navigate("${BleedingEventDetailsDestination.route}/${it}")
+                })
+        }
+        /**
+         * Add new bleeding event
+         */
+        composable(route = BleedingEntryDestination.route) {
+            BleedingEntryScreen()
         }
 
-        composable(route = BleedingEventDestination.route) {
-            BleedingEventScreen()
+        /**
+         * Update bleeding event
+         */
+
+        composable(
+            route = BleedingEventDetailsDestination.routeWithArgs,
+            arguments = listOf(navArgument(BleedingEventDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            BleedingDetailsScreen(navigateToBleedingEdit =
+            { navController.navigate("${BleedingEventEditDestination.route}/${it}") })
         }
+
+        /**
+         * Edit bleeding event
+         */
+
+        composable(
+            route = BleedingEventEditDestination.routeWithArgs,
+            arguments = listOf(navArgument(BleedingEventEditDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            BleedingEditScreen()
+        }
+
 
     }
 }
