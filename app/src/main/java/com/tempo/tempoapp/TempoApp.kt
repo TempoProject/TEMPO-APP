a3815b5c931a25157b0d9cfa568befb0dcec5ab9
Francesco Franco <frafranco16@gmail.com>
2024-03-13 16:07:55 +0100

    Add TempoAppBar

--- app/src/main/java/com/tempo/tempoapp/TempoApp.kt
+++ app/src/main/java/com/tempo/tempoapp/TempoApp.kt
@@ -1,11 +1,47 @@
 package com.tempo.tempoapp
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.tempo.tempoapp.ui.navigation.TempoNavHost
 
 @Composable
-fun TempoApp(navController: NavHostController = rememberNavController()){
+fun TempoApp(navController: NavHostController = rememberNavController()) {
     TempoNavHost(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TempoAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    )
 }
