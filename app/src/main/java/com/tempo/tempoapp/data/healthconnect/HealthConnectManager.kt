8807319691e4c08f4dbb8529d73c055b83e4c715
Francesco Franco <frafranco16@gmail.com>
2024-03-19 15:02:12 +0100

    Implemented HealthConnectManager to handle permissions and data reading.

---
+++ app/src/main/java/com/tempo/tempoapp/data/healthconnect/HealthConnectManager.kt
@@ -0,0 +1,64 @@
+package com.tempo.tempoapp.data.healthconnect
+
+import android.content.Context
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.compose.runtime.mutableStateOf
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.request.ReadRecordsRequest
+import androidx.health.connect.client.time.TimeRangeFilter
+import java.time.Instant
+
+const val MIN_SUPPORTED_SDK = Build.VERSION_CODES.O_MR1
+
+class HealthConnectManager(private val context: Context) {
+    private val healthConnectClient by lazy { HealthConnectClient.getOrCreate(context) }
+
+
+    var availability = mutableStateOf(HealthConnectAvailability.NOT_SUPPORTED)
+        private set
+
+    init {
+        checkAvailability()
+    }
+
+    fun checkAvailability() {
+        availability.value = when {
+            HealthConnectClient.getSdkStatus(context) == HealthConnectClient.SDK_AVAILABLE -> HealthConnectAvailability.INSTALLED
+            isSupported() -> HealthConnectAvailability.NOT_INSTALLED
+            else -> HealthConnectAvailability.NOT_SUPPORTED
+        }
+    }
+
+    private fun isSupported() = Build.VERSION.SDK_INT >= MIN_SUPPORTED_SDK
+
+    suspend fun hasAllPermissions(permissions: Set<String>): Boolean {
+        return healthConnectClient.permissionController.getGrantedPermissions()
+            .containsAll(permissions)
+    }
+
+    fun requestPermissionsActivityContract(): ActivityResultContract<Set<String>, Set<String>> {
+        return PermissionController.createRequestPermissionResultContract()
+    }
+
+    suspend fun readSteps(
+        startTime: Instant,
+        endTime: Instant
+    ): List<StepsRecord> {
+        val request = ReadRecordsRequest(
+            recordType = StepsRecord::class,
+            timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+        )
+        val response = healthConnectClient.readRecords(request)
+        return response.records
+    }
+
+}
+
+enum class HealthConnectAvailability {
+    INSTALLED,
+    NOT_INSTALLED,
+    NOT_SUPPORTED
+}
