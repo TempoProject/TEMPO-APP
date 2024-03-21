6a2249db3be2d42dfb14d5855f659ea2cf16dd14
Francesco Franco <frafranco16@gmail.com>
2024-03-21 10:50:30 +0100

    add stepsRecordRepository

--- app/src/main/java/com/tempo/tempoapp/data/AppContainer.kt
+++ app/src/main/java/com/tempo/tempoapp/data/AppContainer.kt
@@ -3,10 +3,12 @@ package com.tempo.tempoapp.data
 import android.content.Context
 import com.tempo.tempoapp.data.repository.BleedingRepository
 import com.tempo.tempoapp.data.repository.InfusionRepository
+import com.tempo.tempoapp.data.repository.StepsRecordRepository
 
 interface AppContainer {
     val bleedingRepository: BleedingRepository
     val infusionRepository: InfusionRepository
+    val stepsRecordRepository: StepsRecordRepository
 }
 
 class AppDataContainer(private val context: Context) : AppContainer {
@@ -16,6 +18,9 @@ class AppDataContainer(private val context: Context) : AppContainer {
     }
     override val infusionRepository: InfusionRepository by lazy {
         InfusionRepository(TempoDatabase.getDatabase(context).infusionDao())
+    }
+    override val stepsRecordRepository: StepsRecordRepository by lazy {
+        StepsRecordRepository(TempoDatabase.getDatabase(context).stepsDao())
     }
 
 }
