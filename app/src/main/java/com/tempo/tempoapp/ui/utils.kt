e2b8962820ade5b25a6aa503e20a478434716839
Francesco Franco <frafranco16@gmail.com>
2024-03-25 18:02:47 +0100

    add ItemCount: a box to show stats about bleeding, infusion and number steps of the day

--- app/src/main/java/com/tempo/tempoapp/ui/utils.kt
+++ app/src/main/java/com/tempo/tempoapp/ui/utils.kt
@@ -1,5 +1,25 @@
 package com.tempo.tempoapp.ui
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.tempo.tempoapp.R
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -10,3 +30,38 @@ fun Long.toStringDate(): String =
 
 fun Long.toStringTime(): String =
     SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date(this))
+
+
+@Composable
+fun <T> ItemCount(count: T, @DrawableRes iconId: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(dimensionResource(id = R.dimen.padding_medium)))
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painterResource(id = iconId),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+                Text(
+                    text = count.toString(),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+
+        }
+    }
+}
