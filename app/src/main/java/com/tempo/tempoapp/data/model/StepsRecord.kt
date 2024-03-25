7750871a350bb5ab95c6262bbf1c27330a2e2011
Francesco Franco <frafranco16@gmail.com>
2024-03-25 12:12:41 +0100

    add date, change type parameter of startTime and endTime. add extension function toTimestamp

--- app/src/main/java/com/tempo/tempoapp/data/model/StepsRecord.kt
+++ app/src/main/java/com/tempo/tempoapp/data/model/StepsRecord.kt
@@ -3,6 +3,8 @@ package com.tempo.tempoapp.data.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 @Entity(tableName = "steps")
 data class StepsRecord(
@@ -10,8 +12,12 @@ data class StepsRecord(
     val id: Int = 0,
     @ColumnInfo(name = "steps")
     val steps: Long,
+    @ColumnInfo(name = "date")
+    val date: Long,
     @ColumnInfo(name = "startTime")
-    val startTime: String,
+    val startTime: Long,
     @ColumnInfo(name = "endTime")
-    val endTime: String,
+    val endTime: Long,
 )
+
+fun Instant.toTimestamp(unit: ChronoUnit) = this.truncatedTo(unit).toEpochMilli()
