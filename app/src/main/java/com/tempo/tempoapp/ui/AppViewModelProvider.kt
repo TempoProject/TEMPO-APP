6f46679fafe19dec6975c57f6d195a82130083dd
Francesco Franco <frafranco16@gmail.com>
2024-05-24 11:33:19 +0200

    add documentation to AppViewModelProvider

--- app/src/main/java/com/tempo/tempoapp/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/tempo/tempoapp/ui/AppViewModelProvider.kt
@@ -20,8 +20,15 @@ import com.tempo.tempoapp.ui.movesense.ScanDevicesViewModel
 import com.tempo.tempoapp.ui.reminders.ReminderListViewModel
 import com.tempo.tempoapp.ui.reminders.ReminderViewModel
 
+/**
+ * AppViewModelProvider is an object responsible for providing ViewModel instances using Jetpack ViewModelProvider.
+ * It encapsulates the initialization logic for various ViewModel classes used within the Tempo app.
+ */
 object AppViewModelProvider {
 
+    /**
+     * Factory is a ViewModelProvider.Factory instance used for creating ViewModel instances.
+     */
     val Factory = viewModelFactory {
         initializer {
             BleedingEntryViewModel(tempoApplication().container.bleedingRepository)
@@ -33,7 +40,6 @@ object AppViewModelProvider {
                 tempoApplication().container.stepsRecordRepository,
                 tempoApplication().container.movesenseRepository,
                 tempoApplication().healthConnectManager,
-                tempoApplication().workManager
             )
         }
         initializer {
@@ -101,5 +107,10 @@ object AppViewModelProvider {
     }
 }
 
+/**
+ * Extension function to retrieve TempoApplication instance from CreationExtras.
+ *
+ * @return TempoApplication instance.
+ */
 fun CreationExtras.tempoApplication(): TempoApplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TempoApplication)
