4c9b5e276975eccf39f0f6560ff5071c3172802a
Francesco Franco <frafranco16@gmail.com>
2024-05-24 11:40:05 +0200

    add documentation to MovesenseService

--- app/src/main/java/com/tempo/tempoapp/utils/MovesenseService.kt
+++ app/src/main/java/com/tempo/tempoapp/utils/MovesenseService.kt
@@ -22,6 +22,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
+/**
+ * MovesenseService is a Service responsible for managing Movesense device connectivity.
+ * It subscribes to Movesense events and handles notifications for device connection status changes.
+ *
+ * @property mds The Movesense instance used for communication with Movesense devices.
+ * @property mdsSub The Movesense subscription used for receiving device connection events.
+ * @property serviceScope The CoroutineScope used for launching asynchronous tasks.
+ * @property notificationManager The NotificationManager instance used for displaying notifications.
+ */
 class MovesenseService : Service() {
 
     private val mds: Mds = Mds.builder().build(TempoApplication.instance.applicationContext)
@@ -32,6 +41,14 @@ class MovesenseService : Service() {
         TempoApplication.instance.getSystemService(Context.NOTIFICATION_SERVICE) as
                 NotificationManager
 
+    /**
+     * Starts the MovesenseService and handles incoming intents.
+     *
+     * @param intent The Intent supplied to the service.
+     * @param flags Additional data about this start request.
+     * @param startId A unique integer representing this specific request to start.
+     * @return The type of service handling requested, either START_NOT_STICKY, START_STICKY, START_REDELIVER_INTENT.
+     */
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         val movesenseRepository =
@@ -101,17 +118,28 @@ class MovesenseService : Service() {
         return START_NOT_STICKY
     }
 
+    /**
+     * Stops the MovesenseService and cleans up resources.
+     */
     override fun onDestroy() {
         Toast.makeText(this, "Service done", Toast.LENGTH_SHORT).show()
         stopForegroundService()
 
     }
 
-
+    /**
+     * Starts the service in the foreground.
+     */
     private fun startForegroundService() {
         startForeground(2, sendNotification("Movesense collegato"))
     }
 
+    /**
+     * Creates and sends a notification.
+     *
+     * @param content The content of the notification.
+     * @return The notification that was sent.
+     */
     private fun sendNotification(content: String): Notification {
         val title = "Movesense"
         val notification = NotificationCompat.Builder(applicationContext, "Movesense")
@@ -126,6 +154,9 @@ class MovesenseService : Service() {
         return notification
     }
 
+    /**
+     * Stops the service from running in the foreground.
+     */
     private fun stopForegroundService() {
         Log.d(javaClass.simpleName, "unsubscribe")
         mdsSub?.unsubscribe()
