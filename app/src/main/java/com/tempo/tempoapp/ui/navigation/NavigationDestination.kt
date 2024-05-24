8be1f2abbfbd55be06a12c34c359063c74d009c1
Francesco Franco <frafranco16@gmail.com>
2024-05-24 11:31:24 +0200

    add documentation to Navigation + clean code

--- app/src/main/java/com/tempo/tempoapp/ui/navigation/NavigationDestination.kt
+++ app/src/main/java/com/tempo/tempoapp/ui/navigation/NavigationDestination.kt
@@ -1,8 +1,14 @@
 package com.tempo.tempoapp.ui.navigation
 
+/**
+ * Interface representing a navigation destination.
+ * Defines properties for the route and title resource ID.
+ */
 interface NavigationDestination {
 
+    // Unique identifier for the navigation destination
     val route: String
 
+    // Resource ID for the title of the navigation destination
     val titleRes: Int
 }
