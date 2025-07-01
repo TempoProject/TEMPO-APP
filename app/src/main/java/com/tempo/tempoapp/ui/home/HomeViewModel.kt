8e8423a7553fc50b4495c659c0b77d232b345675
ff225 <frafranco16@gmail.com>
2025-07-01 18:22:06 +0200

    refactor HomeScreen and HomeViewModel to improve navigation and event handling

--- app/src/main/java/com/tempo/tempoapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/tempo/tempoapp/ui/home/HomeViewModel.kt
@@ -1,43 +1,21 @@
 package com.tempo.tempoapp.ui.home
 
-import android.app.Application
-import android.app.PendingIntent
-import android.content.Intent
-import androidx.compose.runtime.mutableStateOf
-import androidx.health.connect.client.permission.HealthPermission
-import androidx.health.connect.client.records.BloodGlucoseRecord
-import androidx.health.connect.client.records.BloodPressureRecord
-import androidx.health.connect.client.records.BodyFatRecord
-import androidx.health.connect.client.records.BodyWaterMassRecord
-import androidx.health.connect.client.records.BoneMassRecord
-import androidx.health.connect.client.records.DistanceRecord
-import androidx.health.connect.client.records.ElevationGainedRecord
-import androidx.health.connect.client.records.FloorsClimbedRecord
-import androidx.health.connect.client.records.HeartRateRecord
-import androidx.health.connect.client.records.OxygenSaturationRecord
-import androidx.health.connect.client.records.RespiratoryRateRecord
-import androidx.health.connect.client.records.SleepSessionRecord
-import androidx.health.connect.client.records.StepsRecord
-import androidx.health.connect.client.records.TotalCaloriesBurnedRecord
-import androidx.health.connect.client.records.WeightRecord
-import androidx.lifecycle.AndroidViewModel
+import android.util.Log
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tempo.tempoapp.data.healthconnect.HealthConnectAvailability
-import com.tempo.tempoapp.data.healthconnect.HealthConnectManager
 import com.tempo.tempoapp.data.model.BleedingEvent
 import com.tempo.tempoapp.data.model.InfusionEvent
 import com.tempo.tempoapp.data.model.Movesense
+import com.tempo.tempoapp.data.model.ProphylaxisResponse
 import com.tempo.tempoapp.data.repository.BleedingRepository
 import com.tempo.tempoapp.data.repository.InfusionRepository
 import com.tempo.tempoapp.data.repository.MovesenseRepository
+import com.tempo.tempoapp.data.repository.ProphylaxisResponseRepository
 import com.tempo.tempoapp.data.repository.StepsRecordRepository
-import com.tempo.tempoapp.utils.AlarmManagerHelper
-import com.tempo.tempoapp.utils.StepsReceiver
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
@@ -47,61 +25,54 @@ import java.time.temporal.ChronoUnit
  * @param infusionRepository Repository for infusion events.
  * @param stepsRecordRepository Repository for steps records.
  * @param movesenseRepository Repository for Movesense device information.
- * @param healthConnectManager Health Connect manager.
- * @param application Application context.
  */
 
 class HomeViewModel(
     bleedingRepository: BleedingRepository,
     infusionRepository: InfusionRepository,
     stepsRecordRepository: StepsRecordRepository,
+    prophylaxisResponseRepository: ProphylaxisResponseRepository,
     movesenseRepository: MovesenseRepository,
-    private val healthConnectManager: HealthConnectManager,
-    application: Application
-) : AndroidViewModel(application) {
+    //private val healthConnectManager: HealthConnectManager,
+    //application: Application
+) : ViewModel() {
 
     /**
      * Set of permissions required for reading steps records.
      */
-    val permission = setOf(
-        HealthPermission.getReadPermission(StepsRecord::class),
-        HealthPermission.getReadPermission(HeartRateRecord::class),
-        HealthPermission.getReadPermission(TotalCaloriesBurnedRecord::class),
-        HealthPermission.getReadPermission(BloodGlucoseRecord::class),
-        HealthPermission.getReadPermission(BloodPressureRecord::class),
-        HealthPermission.getReadPermission(BodyFatRecord::class),
-        HealthPermission.getReadPermission(BodyWaterMassRecord::class),
-        HealthPermission.getReadPermission(BoneMassRecord::class),
-        HealthPermission.getReadPermission(DistanceRecord::class),
-        HealthPermission.getReadPermission(ElevationGainedRecord::class),
-        HealthPermission.getReadPermission(FloorsClimbedRecord::class),
-        HealthPermission.getReadPermission(OxygenSaturationRecord::class),
-        HealthPermission.getReadPermission(RespiratoryRateRecord::class),
-        HealthPermission.getReadPermission(SleepSessionRecord::class),
-        HealthPermission.getReadPermission(WeightRecord::class),
-    )
+    //val permission = setOf(
+    //  HealthPermission.getReadPermission(StepsRecord::class),
+    /*HealthPermission.getReadPermission(HeartRateRecord::class),
+    HealthPermission.getReadPermission(TotalCaloriesBurnedRecord::class),
+    HealthPermission.getReadPermission(BloodGlucoseRecord::class),
+    HealthPermission.getReadPermission(BloodPressureRecord::class),
+    HealthPermission.getReadPermission(BodyFatRecord::class),
+    HealthPermission.getReadPermission(BodyWaterMassRecord::class),
+    HealthPermission.getReadPermission(BoneMassRecord::class),
+    HealthPermission.getReadPermission(DistanceRecord::class),
+    HealthPermission.getReadPermission(ElevationGainedRecord::class),
+    HealthPermission.getReadPermission(FloorsClimbedRecord::class),
+    HealthPermission.getReadPermission(OxygenSaturationRecord::class),
+    HealthPermission.getReadPermission(RespiratoryRateRecord::class),
+    HealthPermission.getReadPermission(SleepSessionRecord::class),
+    HealthPermission.getReadPermission(WeightRecord::class)*/
+    //)
 
-    /**
-     * State to keep track of whether the required permissions are granted.
-     */
-    var permissionsGranted = mutableStateOf(false)
-        private set
 
-    /**
-     * Permissions launcher for requesting Health Connect permissions.
-     */
-    val permissionsLauncher = healthConnectManager.requestPermissionsActivityContract()
+    //var permissionsGranted = mutableStateOf(false)
+    //   private set
+
+    //val permissionsLauncher = healthConnectManager.requestPermissionsActivityContract()
 
     init {
-        if (healthConnectManager.availability.value == HealthConnectAvailability.INSTALLED)
-            initialLoad()
+
     }
 
     /**
      * Initial load function to check for permissions and schedule an alarm if permissions are granted.
      */
     fun initialLoad() {
-        viewModelScope.launch {
+        /*viewModelScope.launch {
             permissionsGranted.value =
                 healthConnectManager.hasAllPermissions(permissions = permission)
             println("permission granted in viewmodel? ${permissionsGranted.value}")
@@ -125,13 +96,14 @@ class HomeViewModel(
                     instant
                 )
             }
-        }
+        }*/
     }
 
     /**
      * Combined state flow of UI state containing lists of bleeding events, infusion events,
      * steps count, and movesense device information.
      */
+
     val homeUiState: StateFlow<HomeUiState> =
         combine(
             bleedingRepository.getAllDayBleeding(
@@ -143,10 +115,15 @@ class HomeViewModel(
             stepsRecordRepository.getAllDayStepsCount(
                 Instant.now().truncatedTo(ChronoUnit.DAYS).toEpochMilli()
             ),
+            prophylaxisResponseRepository.getAllDayProphylaxis(
+                Instant.now().truncatedTo(ChronoUnit.DAYS).toEpochMilli()
+            ),
             movesenseRepository.getDevice()
-        ) { bleeding, infusion, steps, movesense ->
+        ) { bleeding, infusion, steps, prophylaxis, movesense ->
             println(movesense)
-            HomeUiState(bleeding, infusion, steps, movesense)
+            Log.d("HomeViewModel", Instant.now().truncatedTo(ChronoUnit.DAYS).toEpochMilli().toString())
+            Log.d("HomeViewModel", "Prophylaxis events: $prophylaxis")
+            HomeUiState(bleeding, infusion, prophylaxis, steps, movesense)
         }.stateIn(
             scope = viewModelScope,
             initialValue = HomeUiState(),
@@ -168,7 +145,20 @@ class HomeViewModel(
  */
 data class HomeUiState(
     val bleedingList: List<BleedingEvent> = listOf(),
-    val infusionList: List<InfusionEvent> = mutableListOf(),
+    val infusionList: List<InfusionEvent> = listOf(),
+    val prophylaxisList: List<ProphylaxisResponse> = listOf(),
     val stepsCount: Int = 0,
     val movesense: Movesense? = null
-)
+) {
+    val combinedEvents: List<HomeEvent>
+        get() = (bleedingList.map { HomeEvent.Bleeding(it) } +
+                infusionList.map { HomeEvent.Infusion(it) }) +
+                prophylaxisList.map { HomeEvent.Prophylaxis(it) }
+                    .sortedByDescending { it.dateTime }
+}
+
+sealed class HomeEvent(val dateTime: Long) {
+    data class Bleeding(val event: BleedingEvent) : HomeEvent(event.timestamp)
+    data class Infusion(val event: InfusionEvent) : HomeEvent(event.timestamp)
+    data class Prophylaxis(val event: ProphylaxisResponse) : HomeEvent(event.reminderDateTime)
+}
